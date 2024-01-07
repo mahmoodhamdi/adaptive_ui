7693a3cf45b86bea25bb0b26b42c083bae9812f2
mahmoodhamdi <mahmouashwah@gmail.com>
2024-01-07 18:17:25 +0200

    create the home view with app bar

---
+++ lib/views/home_view.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class HomeView extends StatelessWidget {
+  const HomeView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        leading: IconButton(
+          icon: const Icon(
+            Icons.menu,
+            color: Colors.white,
+          ),
+          onPressed: () {},
+        ),
+      ),
+    );
+  }
+}
